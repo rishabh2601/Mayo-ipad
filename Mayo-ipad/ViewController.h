70b704e8b75584503cb81308a7e390fe498ec244
rishabh2601 <rsrivas5@asu.edu>
2014-10-08 15:59:54 -0700

    Initial Commit

---
+++ Mayo-ipad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mayo-ipad
+//
+//  Created by Rishabh Srivastava on 08/10/14.
+//  Copyright (c) 2014 rishabh srivastava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Mayo-ipad/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Mayo-ipad
+//
+//  Created by Rishabh Srivastava on 08/10/14.
+//  Copyright (c) 2014 rishabh srivastava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
